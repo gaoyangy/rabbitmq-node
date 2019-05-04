9fca04b974c0edf1262680caf3974e4a1540c527
alien-wind <13373935140@163.com>
2019-05-05 01:56:22 +0800

    add

--- app/queue/producer.js
+++ app/queue/producer.js
@@ -1,12 +1,19 @@
 const amqplib = require('amqplib');
-
+let times = 1
 exports.push = function() {
   amqplib.connect('amqp://localhost').then(async conn => {
     try {
       const channel = await conn.createChannel();
       await channel.assertQueue('/api', { durable: false });
-      await channel.sendToQueue('/api', Buffer.from('hello'));
-      console.log(" [x] Sent '%s'", 123456);
+      const ch = await channel.sendToQueue('/api', Buffer.from('hello'));
+      let mqtime = setInterval(() => {
+        this.push()
+        ++ times
+      }, 1000)
+      setTimeout(() => {
+        conn.close()
+      },10)
+      console.log(" [x] Sent '%s'");
     } catch (error) {
       console.log(error);
     }
